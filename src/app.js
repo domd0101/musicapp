59f59132bad2cf544723809280d8fb1b594ddf6b
domd0101 <ddecicco@buffalo.edu>
2017-07-04 16:41:02 -0600

    first commit

--- src/app.js
+++ src/app.js
@@ -6,25 +6,39 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      query:''
+      query:'',
+      info:'',
+      name:'',
+      weather:'',
+      temp:''
     }
   }
 
-  search(){
-    console.log('this.state', this.state);
+  search() {
+    const FETCH_URL = `http://api.openweathermap.org/data/2.5/weather?zip=${  this.state.query},us&APPID=061f24cf3cde2f60644a8240302983f2`
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then((res)=> res.json())
+    .then((json)=>{
+      this.setState({info:json});
+      console.log('location',this.state.info);
+      this.setState({name:this.state.info.name})
+      this.setState({temp:(Number(this.state.info.main.temp)*(9/5)-459.67).toFixed(2)})
+      this.setState({weather:this.state.info.weather[0].main})
+    })
   }
-
 
 
   render(){
     return(
       <section className='app'>
-        <div className='app-title'>Music Master React App</div>
+        <div className='app-title'>Weather React App</div>
         <FormGroup>
           <InputGroup>
             <FormControl
               type='text'
-              placeholder='search an Artist...'
+              placeholder='search an zip...'
               value={this.state.query}
               onChange={event => {this.setState({query:event.target.value})}}
               onKeyPress={event => {
@@ -39,12 +53,9 @@ class App extends Component {
          </InputGroup>
         </FormGroup>
         <div className='profile'>
-          <div>Artist Name</div>
-          <div>Artist Pic</div>
-          <div>Artist Songs</div>
-          <div className='gallery'>
-
-          </div>
+          <div>City: {this.state.name}</div>
+          <div>Weather: {this.state.weather}</div>
+          <div>Temp: {this.state.temp}</div>
         </div>
 
 
